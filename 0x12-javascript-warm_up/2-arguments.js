35551e2b584cdd0fe262f9cc526ca861d6997bc7
Stephen <stephok5@gmail.com>
2023-03-14 09:39:35 +0000

    Argumesnt in JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@
 // A script that arguments are passed
 
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Argument found');
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
