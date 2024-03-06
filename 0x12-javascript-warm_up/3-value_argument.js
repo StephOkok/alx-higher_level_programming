ea1b6b66152ebbe1615b6b8a6cedf9b75d46c5e1
Stephen Okok <107418951+StephOkok@users.noreply.github.com>
2024-03-06 13:08:45 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-// A sript that prints argument
-
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-// Concatenating two srings in JS
-
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
